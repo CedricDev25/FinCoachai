f1dc0fa82c70e178ff2a12e135cee58139a0bfa2
Imena Cedric <162682901+CedricDev25@users.noreply.github.com>
2026-05-02 11:25:02 +0200

    Add initial script.js with project modal functionality

---
+++ script.js
@@ -0,0 +1,316 @@
+"use strict";
+(self.webpackChunkmian_project = self.webpackChunkmian_project || []).push([["428"], {
+    62414: function(e, t, s) {
+        s.r(t),
+        s.d(t, {
+            default: function() {
+                return w
+            }
+        });
+        var r = s("10239")
+          , l = s("8600")
+          , o = s("37966")
+          , n = s("59890")
+          , a = s("241")
+          , i = s("84145")
+          , c = s("28866")
+          , d = s("52721")
+          , C = s("47431")
+          , p = {
+            exportProjectZipModal: "exportProjectZipModal-rX4j84",
+            paymentSuccessIcon: "paymentSuccessIcon-XGxxh_",
+            modalContent: "modalContent-twm924",
+            modalTitle: "modalTitle-g2xjJW",
+            modalSubtitle: "modalSubtitle-rUTQvC",
+            urlList: "urlList-B0AVPJ",
+            urlItem: "urlItem-H9wb2z",
+            urlText: "urlText-OHiUn6",
+            progressSection: "progressSection-_dkYx6",
+            progressBar: "progressBar-Y0ANsk",
+            progressFill: "progressFill-B8K9Tm",
+            progressText: "progressText-uwaVHJ",
+            planRestriction: "planRestriction-_TGjI_",
+            footerButtons: "footerButtons-zKkZRb",
+            downloadButton: "downloadButton-Z8kfMP",
+            upgradeButton: "upgradeButton-ndf15f",
+            lightningIcon: "lightningIcon-bvs79O"
+        }
+          , u = s("32391")
+          , h = s("63589");
+        let x = e => (0,
+        r.jsxs)("svg", {
+            xmlns: "http://www.w3.org/2000/svg",
+            width: 16,
+            height: 16,
+            fill: "none",
+            viewBox: "0 0 16 16",
+            ...e,
+            children: [(0,
+            r.jsx)("path", {
+                stroke: "currentColor",
+                strokeWidth: 1.133,
+                d: "M11.333 1.667H4.666c-.552 0-1 .425-1 .95v10.766c0 .525.448.95 1 .95h6.667c.552 0 1-.425 1-.95V2.617c0-.525-.448-.95-1-.95Z"
+            }), (0,
+            r.jsx)("path", {
+                stroke: "currentColor",
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: 1.133,
+                d: "M6.666 12.333h2.667"
+            })]
+        })
+          , m = () => (0,
+        r.jsxs)("svg", {
+            xmlns: "http://www.w3.org/2000/svg",
+            width: "12",
+            height: "12",
+            viewBox: "0 0 12 12",
+            className: "shrink-0 w-4 h-4 fill-[rgba(255,255,255,0.9)]  group-hover:fill-[#11E097]",
+            children: [(0,
+            r.jsx)("g", {
+                clipPath: "url(#clip0_22_85)",
+                children: (0,
+                r.jsx)("path", {
+                    d: "M6 0.5C9.03757 0.5 11.5 2.96243 11.5 6C11.5 9.03757 9.03757 11.5 6 11.5C2.96243 11.5 0.5 9.03757 0.5 6C0.5 2.96243 2.96243 0.5 6 0.5ZM7.23047 8.43848C6.83391 8.47694 6.42199 8.5 6 8.5C5.57766 8.5 5.16541 8.47701 4.76855 8.43848C4.84852 8.77424 4.94219 9.08053 5.0498 9.34961C5.21682 9.76716 5.40359 10.0719 5.58496 10.2637C5.76526 10.4543 5.90567 10.5 6 10.5C6.09433 10.5 6.23474 10.4543 6.41504 10.2637C6.59641 10.0719 6.78318 9.76716 6.9502 9.34961C7.05779 9.08059 7.15051 8.77416 7.23047 8.43848ZM1.82129 7.66895C2.27783 8.811 3.18818 9.72088 4.33008 10.1777C4.25568 10.0331 4.18575 9.88083 4.12207 9.72168C3.95353 9.30033 3.81621 8.81519 3.71289 8.28613C3.18452 8.18288 2.70021 8.04722 2.2793 7.87891C2.11951 7.81499 1.96641 7.74367 1.82129 7.66895ZM10.1777 7.66895C10.0329 7.74348 9.8811 7.81513 9.72168 7.87891C9.30038 8.04743 8.81512 8.18282 8.28613 8.28613C8.18282 8.81512 8.04743 9.30038 7.87891 9.72168C7.81513 9.8811 7.74348 10.0329 7.66895 10.1777C8.81112 9.72104 9.72104 8.81112 10.1777 7.66895ZM6 4.5C5.5039 4.5 5.02834 4.52911 4.58398 4.58398C4.52911 5.02834 4.5 5.5039 4.5 6C4.5 6.49574 4.52919 6.97096 4.58398 7.41504C5.02837 7.46992 5.50385 7.5 6 7.5C6.49579 7.5 6.97093 7.46984 7.41504 7.41504C7.46984 6.97093 7.5 6.49579 7.5 6C7.5 5.50385 7.46992 5.02837 7.41504 4.58398C6.97096 4.52919 6.49574 4.5 6 4.5ZM3.56055 4.76855C3.22514 4.84848 2.91923 4.94229 2.65039 5.0498C2.23284 5.21682 1.92811 5.40359 1.73633 5.58496C1.54574 5.76526 1.5 5.90567 1.5 6C1.5 6.09433 1.54574 6.23474 1.73633 6.41504C1.92811 6.59641 2.23284 6.78318 2.65039 6.9502C2.91917 7.05769 3.22523 7.15056 3.56055 7.23047C3.52208 6.83394 3.5 6.42196 3.5 6C3.5 5.5777 3.52202 5.16538 3.56055 4.76855ZM8.43848 4.76855C8.47701 5.16541 8.5 5.57766 8.5 6C8.5 6.42199 8.47694 6.83391 8.43848 7.23047C8.77416 7.15051 9.08059 7.05779 9.34961 6.9502C9.76716 6.78318 10.0719 6.59641 10.2637 6.41504C10.4543 6.23474 10.5 6.09433 10.5 6C10.5 5.90567 10.4543 5.76526 10.2637 5.58496C10.0719 5.40359 9.76716 5.21682 9.34961 5.0498C9.08053 4.94219 8.77424 4.84852 8.43848 4.76855ZM4.33008 1.82129C3.1883 2.27798 2.27798 3.1883 1.82129 4.33008C1.96629 4.25543 2.11965 4.18593 2.2793 4.12207C2.70034 3.95368 3.1843 3.81617 3.71289 3.71289C3.81617 3.1843 3.95368 2.70034 4.12207 2.2793C4.18593 2.11965 4.25543 1.96629 4.33008 1.82129ZM7.66895 1.82129C7.74367 1.96641 7.81499 2.11951 7.87891 2.2793C8.04722 2.70021 8.18288 3.18452 8.28613 3.71289C8.81519 3.81621 9.30033 3.95353 9.72168 4.12207C9.88083 4.18575 10.0331 4.25568 10.1777 4.33008C9.72088 3.18818 8.811 2.27783 7.66895 1.82129ZM6 1.5C5.90567 1.5 5.76526 1.54574 5.58496 1.73633C5.40359 1.92811 5.21682 2.23284 5.0498 2.65039C4.94229 2.91923 4.84848 3.22514 4.76855 3.56055C5.16538 3.52202 5.5777 3.5 6 3.5C6.42196 3.5 6.83394 3.52208 7.23047 3.56055C7.15056 3.22523 7.05769 2.91917 6.9502 2.65039C6.78318 2.23284 6.59641 1.92811 6.41504 1.73633C6.23474 1.54574 6.09433 1.5 6 1.5Z"
+                })
+            }), (0,
+            r.jsx)("defs", {
+                children: (0,
+                r.jsx)("clipPath", {
+                    id: "clip0_22_85",
+                    children: (0,
+                    r.jsx)("rect", {
+                        width: "12",
+                        height: "12"
+                    })
+                })
+            })]
+        })
+          , g = {
+            frontend: m,
+            app: () => (0,
+            r.jsx)(x, {
+                className: "shrink-0 w-4 h-4 text-[rgba(255,255,255,0.9)]  group-hover:text-[#11E097]"
+            }),
+            backend: () => (0,
+            r.jsx)("svg", {
+                xmlns: "http://www.w3.org/2000/svg",
+                width: "12",
+                height: "12",
+                viewBox: "0 0 12 12",
+                className: "shrink-0 w-4 h-4 fill-[rgba(255,255,255,0.9)]  group-hover:fill-[#11E097]",
+                children: (0,
+                r.jsx)("path", {
+                    d: "M9.8252 1C10.474 1.00011 10.9999 1.52603 11 2.1748V9.8252C10.9999 10.474 10.474 10.9999 9.8252 11H2.1748C1.52603 10.9999 1.00011 10.474 1 9.8252V2.1748C1.00011 1.52603 1.52603 1.00011 2.1748 1H9.8252ZM2 9.8252C2.00011 9.92169 2.07831 9.9999 2.1748 10H9.8252C9.9217 9.99989 9.99989 9.9217 10 9.8252V8H2V9.8252ZM3.25 8.5C3.52614 8.5 3.75 8.72386 3.75 9C3.75 9.27614 3.52614 9.5 3.25 9.5H3C2.72386 9.5 2.5 9.27614 2.5 9C2.5 8.72386 2.72386 8.5 3 8.5H3.25ZM9 8.5C9.27614 8.5 9.5 8.72386 9.5 9C9.5 9.27614 9.27614 9.5 9 9.5H6.5C6.22386 9.5 6 9.27614 6 9C6 8.72386 6.22386 8.5 6.5 8.5H9ZM2.1748 2C2.07831 2.00011 2.00011 2.07831 2 2.1748V7H10V2.1748C9.9999 2.07831 9.92169 2.00011 9.8252 2H2.1748Z"
+                })
+            })
+        };
+        var w = function({visible: e, onClose: t, isFreePlan: s, projectId: x, upgradeRef: w, onReopenAfterUpgrade: j}) {
+            let {messageData: v} = (0,
+            c.Ki)(null)
+              , {previewVersionId: b} = (0,
+            d.tT)("platform/generate")
+              , [f,k] = (0,
+            l.useState)(!1)
+              , {progress: Z, start: _, applyRealProgress: M, complete: N, reset: B} = function(e) {
+                let {fakeMax: t=95, interval: s=300, minStep: r=1, maxStep: o=4} = e || {}
+                  , [n,a] = (0,
+                l.useState)(0)
+                  , [i,c] = (0,
+                l.useState)(!1)
+                  , d = (0,
+                l.useRef)(null)
+                  , C = () => {
+                    null != d.current && (window.clearInterval(d.current),
+                    d.current = null)
+                }
+                  , p = (0,
+                l.useCallback)( () => {
+                    !i && (c(!0),
+                    a(0),
+                    C(),
+                    d.current = window.setInterval( () => {
+                        a(e => e >= t ? e : Math.floor(Math.min(e + (Math.random() * (o - r) + r), t)))
+                    }
+                    , s))
+                }
+                , [i, t, s, r, o])
+                  , u = (0,
+                l.useCallback)(e => {
+                    a(s => {
+                        let r = Math.min(e, t);
+                        return r <= s ? s : r
+                    }
+                    )
+                }
+                , [t])
+                  , h = (0,
+                l.useCallback)( () => {
+                    C(),
+                    c(!1),
+                    a(100)
+                }
+                , [])
+                  , x = (0,
+                l.useCallback)( () => {
+                    C(),
+                    c(!1),
+                    a(0)
+                }
+                , []);
+                return (0,
+                l.useEffect)( () => () => {
+                    C()
+                }
+                , []),
+                {
+                    progress: n,
+                    running: i,
+                    start: p,
+                    applyRealProgress: u,
+                    complete: h,
+                    reset: x
+                }
+            }({
+                fakeMax: 95,
+                interval: 300,
+                minStep: 1,
+                maxStep: 4
+            })
+              , P = v?.getValidUrls ?? []
+              , T = v?.getAutoCompleteUrls ?? []
+              , H = async () => {
+                if (!f) {
+                    k(!0),
+                    B(),
+                    _();
+                    try {
+                        await (0,
+                        C.RP)(x, b ?? 0, e => {
+                            M(e)
+                        }
+                        ),
+                        N(),
+                        setTimeout( () => {
+                            t(),
+                            k(!1),
+                            B()
+                        }
+                        , 500)
+                    } catch (e) {
+                        i.xh.error(o.ZP.get("export_zip_downloadFailed")),
+                        k(!1),
+                        B()
+                    }
+                }
+            }
+            ;
+            return (0,
+            r.jsx)(n.Z, {
+                visible: e,
+                onClose: t,
+                width: 520,
+                className: p.exportProjectZipModal,
+                closable: !0,
+                footer: null,
+                title: (0,
+                r.jsxs)("div", {
+                    className: "flex gap-4 items-center",
+                    children: [(0,
+                    r.jsx)("img", {
+                        className: p.paymentSuccessIcon,
+                        src: h
+                    }), (0,
+                    r.jsx)("div", {
+                        className: p.modalTitle,
+                        children: o.ZP.get("export_zip_title")
+                    })]
+                }),
+                children: (0,
+                r.jsxs)("div", {
+                    className: p.modalContent,
+                    children: [(0,
+                    r.jsx)("div", {
+                        className: p.modalSubtitle,
+                        children: o.ZP.get("export_zip_subtitle")
+                    }), (0,
+                    r.jsx)("div", {
+                        className: p.urlList,
+                        children: P.map( ([e,t], s) => {
+                            let l = g[e] ?? m
+                              , o = T[s] || "-";
+                            return (0,
+                            r.jsxs)("div", {
+                                className: `${p.urlItem} group cursor-pointer`,
+                                onClick: () => {
+                                    "app" === e ? window.open(`${o}?mobile=1`, "_blank") : window.open(`${o}`, "_blank")
+                                }
+                                ,
+                                children: [(0,
+                                r.jsx)(l, {}), (0,
+                                r.jsx)("span", {
+                                    className: `${p.urlText} group-hover:text-[#11E097]`,
+                                    title: `${o}`,
+                                    children: o
+                                })]
+                            }, e)
+                        }
+                        )
+                    }), f && (0,
+                    r.jsxs)("div", {
+                        className: p.progressSection,
+                        children: [(0,
+                        r.jsx)("div", {
+                            className: p.progressBar,
+                            children: (0,
+                            r.jsx)("div", {
+                                className: p.progressFill,
+                                style: {
+                                    width: `${Z}%`
+                                }
+                            })
+                        }), (0,
+                        r.jsx)("div", {
+                            className: p.progressText,
+                            children: o.ZP.get("export_zip_downloading", {
+                                progress: Z
+                            })
+                        })]
+                    }), s && (0,
+                    r.jsx)("div", {
+                        className: p.planRestriction,
+                        children: o.ZP.get("export_zip_planRestriction")
+                    }), (0,
+                    r.jsx)("div", {
+                        className: p.footerButtons,
+                        children: s ? (0,
+                        r.jsxs)("button", {
+                            type: "button",
+                            className: p.upgradeButton,
+                            onClick: () => {
+                                j && u.v.push(j),
+                                t(),
+                                w?.current?.setVisible(!0)
+                            }
+                            ,
+                            children: [(0,
+                            r.jsx)(a.Tm, {
+                                className: p.lightningIcon
+                            }), o.ZP.get("export_zip_upgradePlan"), "                            "]
+                        }) : (0,
+                        r.jsx)("button", {
+                            type: "button",
+                            "data-matomo-event": "click",
+                            "data-matomo-category": "project_download_button",
+                            "data-matomo-action": "download_button",
+                            className: p.downloadButton,
+                            onClick: H,
+                            disabled: f,
+                            children: o.ZP.get("export_zip_download")
+                        })
+                    })]
+                })
+            })
+        }
+    }
+}]);
